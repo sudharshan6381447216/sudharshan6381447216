0cd1089e306ad460b19bf8184dce1d2ed56ce2b1
Jagadeesh Himayan <>
2022-06-30 18:59:15 +0530

    Sudharshan M.Tech Software Engineering Student at VIT Vellore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sudharshan6381447216
- Wolkus-Online-Assessment
+# Movie Recommender using ReactiveX
+> This is a movie recommender application based on ReactiveX concept. I create 3 streams to fetch movie data from API endpoints and each stream is considered as distinct. So the user can remove a single suggestion and it will be replaced by a new suggestion.
+
+## Features
+- [x] When user open the page, it renders 3 movie suggestions via calling from API endpoints.
+- [x] When user click 'show me more' button, it re-render 3 new movie suggestions.
+- [x] When user click 'x' button on one of the movie suggestion, it replaces with a new suggestion.
+- [x] When user click movie title, a new tab of movie homepage will show up.
+- [x] When user click 'add to cart' on one of the movie suggestions, the movie will be added to user's cart(collections) which is located on the right up corner of page.
+- [x] User's cart is saved in local storage(offline) so user can still access that when revisiting page.
---
+++ movie.js
@@ -0,0 +1,163 @@
+var API_KEY = 'fd2011fbe1938026b40fcf7cdc3f75ab';
+
+var refreshButton = document.querySelector('.refresh');
+var closeButton1 = document.querySelector('.close1');
+var closeButton2 = document.querySelector('.close2');
+var closeButton3 = document.querySelector('.close3');
+
+
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+var close1ClickStream = Rx.Observable.fromEvent(closeButton1, 'click');
+var close2ClickStream = Rx.Observable.fromEvent(closeButton2, 'click');
+var close3ClickStream = Rx.Observable.fromEvent(closeButton3, 'click');
+
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+// localStorage.setObj('movie_cart',[]);
+var cart;
+window.cart = cart;
+//init dropdown
+function initDropdown(){
+    //initialize localstorage
+    cart = localStorage.getObj('movie_cart');
+    if(!cart){
+        cart = [];
+        localStorage.setObj('movie_cart',[]);
+    }
+    // console.log(cart);
+
+    var myDropdown = document.getElementById('myDropdown');
+    document.getElementById('drop_btn').textContent= 'Your Cart('+cart.length+')';
+    while (myDropdown.hasChildNodes()) {
+        myDropdown.removeChild(myDropdown.lastChild);
+    }
+    var cart_length = localStorage.getObj('movie_cart').length;
+    for(var i = 0; i < cart_length; i++){
+        var id = cart[i].split('#')[0];
+        var imgsrc = cart[i].split('#')[1];
+
+        var child = document.createElement('div');
+        var img = document.createElement('img');
+        // var imgEl = document.createElement('img');
+        img.src = 'https://image.tmdb.org/t/p/w500'+ imgsrc;
+        img.textContent = i;
+        img.id = id;
+        img.addEventListener('click',function(){
+            window.open('https://www.themoviedb.org/movie/'+this.id,'_blank');
+        })
+
+        var x = document.createElement('a');
+        x.textContent = 'X';
+        x.id = id;
+        x.addEventListener('click',function(){
+            // console.log(this.id);
+            var this_id = this.id;
+            var newcart = cart.filter(function(c){
+                return !c.startsWith(this_id.toString());
+            })
+            localStorage.setObj('movie_cart',newcart);
+            //re-render dropdown
+            initDropdown();
+            return;
+        })
+        child.appendChild(img);
+        child.appendChild(x);
+        myDropdown.appendChild(child);
+
+    }
+}
+
+initDropdown();
+
+//dropdown function
+function dropFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+var requestStream = refreshClickStream.startWith('startup click')
+    .map(function(){
+        var page = Math.floor(Math.random()*100)+1
+        var randomOffset = Math.floor(Math.random()*20);
+        return 'https://api.themoviedb.org/3/discover/movie?api_key='+API_KEY+'&page='+page;
+    })
+
+var responseStream = requestStream
+    .flatMap(function (requestUrl) {
+        return Rx.Observable.fromPromise($.getJSON(requestUrl));
+    });
+
+function createSuggestionStream(closeClickStream) {
+    return closeClickStream.startWith('startup click')
+        .combineLatest(responseStream,             
+            function(click, listMovies) {
+                return listMovies['results'][Math.floor(Math.random()*20)];
+            }
+        )
+        .merge(
+            refreshClickStream.map(function(){ 
+                return null;
+            })
+        )
+        .startWith(null);
+}
+
+var suggestion1Stream = createSuggestionStream(close1ClickStream);
+var suggestion2Stream = createSuggestionStream(close2ClickStream);
+var suggestion3Stream = createSuggestionStream(close3ClickStream);
+
+
+// Rendering ---------------------------------------------------
+function renderSuggestion(suggestedMovie, selector) {
+    var suggestionEl = document.querySelector(selector);
+    if (suggestedMovie === null) {
+        suggestionEl.style.visibility = 'hidden';
+    } else {
+        suggestionEl.style.visibility = 'visible';
+        var movienameEl = suggestionEl.querySelector('.moviename');
+        movienameEl.href = 'https://www.themoviedb.org/movie/'+suggestedMovie.id;
+        movienameEl.textContent = suggestedMovie.title;
+        var imgEl = suggestionEl.querySelector('img');
+        imgEl.src = "";
+        imgEl.src = 'https://image.tmdb.org/t/p/w500'+ suggestedMovie.poster_path;
+
+        var overviewEl = suggestionEl.querySelector('.overview');
+        overviewEl.textContent = suggestedMovie.overview;
+
+        var releaseEL = suggestionEl.querySelector('.release_date');
+        releaseEL.textContent = suggestedMovie.release_date;
+
+        var voteEL = suggestionEl.querySelector('.vote_average');
+        voteEL.textContent = suggestedMovie.vote_average+'/10';
+
+        var addToCartEl = suggestionEl.querySelector('.addToCart');
+
+        // dirty way to solve the 'overlap-element' bug
+        addToCartEl.id = suggestedMovie.id+'#'+suggestedMovie.poster_path;
+        addToCartEl.addEventListener("click", function(){
+            // console.log(suggestedMovie.id);
+            if(cart.indexOf(this.id) < 0){
+                cart.push(this.id);
+                localStorage.setObj('movie_cart',cart);
+                //re-render dropdown
+                initDropdown();
+            }
+        });
+    }
+}
+
+suggestion1Stream.subscribe(function (suggestedMovie) {
+    renderSuggestion(suggestedMovie, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(function (suggestedMovie) {
+    renderSuggestion(suggestedMovie, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(function (suggestedMovie) {
+    renderSuggestion(suggestedMovie, '.suggestion3');
+});
